d26a6ccfc6428f2c44122d8d4525a982d090f428
UpendraVarmaBadarvada <46284493+UpendraVarmaBadarvada@users.noreply.github.com>
2024-03-26 04:41:29 +0530

    Update reviews.js

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -4,6 +4,7 @@ const Review = require('../models/review');
 module.exports.createReview = async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    // we get current user id from user._id  
     review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
@@ -14,6 +15,7 @@ module.exports.createReview = async (req, res) => {
 
 module.exports.deleteReview = async (req, res) => {
     const { id, reviewId } = req.params;
+    // pull operator removes from an existing array all instances of a value or values that match a specified condition.
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
     req.flash('success', 'Successfully deleted review')
