0616eedc0c5810b876097c47dcc7bc9b51d88410
UpendraVarmaBadarvada <46284493+UpendraVarmaBadarvada@users.noreply.github.com>
2024-03-25 23:41:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ db.once("open", () => {
 });
 
 const app = express();
-
+// we use ejs-mate for using boilerplate code  
 app.engine('ejs', ejsMate)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
