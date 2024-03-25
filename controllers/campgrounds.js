ead4ec8a9fb622153b65087db0b6d472573a62c4
UpendraVarmaBadarvada <46284493+UpendraVarmaBadarvada@users.noreply.github.com>
2024-03-26 05:26:57 +0530

    Update campgrounds.js

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -22,6 +22,7 @@ module.exports.createCampground = async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     campground.geometry = geoData.body.features[0].geometry;
     campground.images = req.files.map(f => ({ url: f.path, filename: f.filename }));
+    // we get current user id from user._id  
     campground.author = req.user._id;
     await campground.save();
     console.log(campground);
