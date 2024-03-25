546efabe4ea832ec92fcd5fe725b5bade8496e29
UpendraVarmaBadarvada <46284493+UpendraVarmaBadarvada@users.noreply.github.com>
2024-03-26 03:31:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -131,6 +131,7 @@ passport.use(new LocalStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
+// flash message types defined here 
 app.use((req, res, next) => {
     res.locals.currentUser = req.user;
     res.locals.success = req.flash('success');
