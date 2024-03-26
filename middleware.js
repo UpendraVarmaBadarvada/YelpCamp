b9435260ec7d5eddc1b92117854fb55547f00988
UpendraVarmaBadarvada <46284493+UpendraVarmaBadarvada@users.noreply.github.com>
2024-03-26 07:05:05 +0530

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -39,6 +39,7 @@ module.exports.isAuthor = async (req, res, next) => {
 
 module.exports.isReviewAuthor = async (req, res, next) => {
     const { id, reviewId } = req.params;
+    // check if review author is current user
     const review = await Review.findById(reviewId);
     if (!review.author.equals(req.user._id)) {
         req.flash('error', 'You do not have permission to do that!');
